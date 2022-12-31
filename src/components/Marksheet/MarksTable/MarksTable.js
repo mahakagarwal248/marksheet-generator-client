f22ea518760bb5c264a2faa6a4dc49212ad788ad
mahakagarwal248 <mahak.agarwal@techchefz.com>
2022-12-31 18:55:16 +0530

    frontend modified

--- src/components/Marksheet/MarksTable/MarksTable.js
+++ src/components/Marksheet/MarksTable/MarksTable.js
@@ -7,10 +7,10 @@ function MarksTable() {
       <Table bordered hover>
         <thead style={{border:'2px solid black'}}>
           <tr>
-            <th style={{ width: "60%", verticalAlign:'middle' }}>Subject Code & Name</th>
-            <th style={{ textAlign: "center", padding: 0, width: "12%", border:'2px solid black' }}>
+            <th style={{ width: "50%", verticalAlign:'middle' }}>Subject Code & Name</th>
+            <th style={{ textAlign: "center", padding: 0, width: "17%", border:'2px solid black' }}>
               <tr style={{ display: "grid", borderBottom: "2px solid black" }}>
-                <th>Maximum Marks</th>
+                <th>Maximum Ma</th>
               </tr>
               <tr style={{ display: "flex" }}>
                 <th style={{ width: "50%", borderRight: "2px solid black" }}>
@@ -20,7 +20,7 @@ function MarksTable() {
               </tr>
             </th>
             <th style={{ width: "8%", textAlign: "center",border:'2px solid black',verticalAlign:'middle' }}>Total</th>
-            <th style={{ textAlign: "center", padding: 0, width: "12%",border:'2px solid black' }}>
+            <th style={{ textAlign: "center", padding: 0, width: "17%",border:'2px solid black' }}>
               <tr style={{ display: "grid", borderBottom: "2px solid black" }}>
                 <th>Marks Obtained</th>
               </tr>
@@ -36,134 +36,15 @@ function MarksTable() {
         </thead>
         <tbody style={{border:'2px solid black'}}>
           <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
+            <td style={{ width: "50%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
+            <td style={{ textAlign: "center", width: "17%",border:'2px solid black',padding:0 }}>
               <tr style={{ display: "flex" }}>
                 <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
                 <td style={{ width: "50%" }}>50</td>
               </tr>
             </td>
             <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
-                <td style={{ width: "50%" }}>42</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0,width:'8%'}}>100</td>
-          </tr>
-          <tr>
-            <td style={{ width: "60%",border:'2px solid black',padding:'0px 8px' }}>Operating System BCST-501</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
-              <tr style={{ display: "flex" }}>
-                <td style={{ width: "50%", borderRight:'2px solid black' }}>100</td>
-                <td style={{ width: "50%" }}>50</td>
-              </tr>
-            </td>
-            <td style={{textAlign:'center',border:'2px solid black',padding:0, width:'8%'}}>Otto</td>
-            <td style={{ textAlign: "center", width: "12%",border:'2px solid black',padding:0 }}>
+            <td style={{ textAlign: "center", width: "17%",border:'2px solid black',padding:0 }}>
               <tr style={{ display: "flex" }}>
                 <td style={{ width: "50%", borderRight:'2px solid black' }}>98</td>
                 <td style={{ width: "50%" }}>42</td>
